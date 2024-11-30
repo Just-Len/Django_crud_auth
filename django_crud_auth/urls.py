ef10d1dcd9bb7bfe5c25db52ae3437e7c43cce74
Len <linakamado@gmail.com>
2024-11-29 22:40:35 -0600

    More details in login and register

--- django_crud_auth/urls.py
+++ django_crud_auth/urls.py
@@ -20,6 +20,8 @@ from task import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('tasks/', views.tasks, name = 'tasks'),
+    
 ]
